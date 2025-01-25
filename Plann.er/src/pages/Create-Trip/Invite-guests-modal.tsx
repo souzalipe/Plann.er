fd1fd28d1041625dfa981cc693050fcbbd81faa8
Felipe Nascimento de Souza <fn23886@gmail.com>
2025-01-25 10:23:31 -0300

    refatora modais de convite e confirmação de viagem; melhora a estrutura e a legibilidade do código

--- Plann.er/src/pages/Create-Trip/Invite-guests-modal.tsx
+++ Plann.er/src/pages/Create-Trip/Invite-guests-modal.tsx
@@ -1,63 +1,69 @@
-import { AtSign, Plus, X } from "lucide-react";
+import { X, AtSign, Plus } from "lucide-react"
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface InviteGuestsModalProps {
-  emailsToInvite: string[];
-  removeEmailToInvite: (email: string) => void;
-  addNewEmailToInvite: (event: FormEvent<HTMLFormElement>) => void;
   closeGuestsModal: () => void;
+  emailsToInvite: string[];
+  addNewEmailToInvite: (event: FormEvent<HTMLFormElement>) => void;
+  removeEmailFromInvites: (email: string) => void;
 }
 
-export function InviteGuestsModal ({
-    addNewEmailToInvite,
-    closeGuestsModal,
-    emailsToInvite,
-    removeEmailToInvite
-}: InviteGuestsModalProps): JSX.Element {
+export function InviteGuestsModal({ 
+  closeGuestsModal, 
+  addNewEmailToInvite, 
+  emailsToInvite, 
+  removeEmailFromInvites 
+}: InviteGuestsModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="font-lg font-semibold">Selecionar convidados</h2>
+            <button>
+              <X className="size-5 text-zinc-400" onClick={closeGuestsModal} />
+            </button>
+          </div>
 
-    return <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-        <div className="w-[640px] rounded-xl space-y-5 py-5 px-6 shadow-[0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03)] bg-zinc-900">
-            <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                    <h2 className="text-lg font-semibold">Selecionar convidados</h2>
-                    <button onClick={closeGuestsModal}>
-                        <X className="size-5 text-zinc-400" />
-                    </button>
-                </div>
-                <p className="text-sm text-zinc-400">
-                    Os convidados irão receber e-mails para confirmar a participação
-                    na viagem.
-                </p>
-            </div>
-
-            <div className="flex flex-wrap gap-2">
-                {emailsToInvite.map((email) => (
-                    <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2" key={email}>
-                        <span className="text-zinc-300">{email}</span>
-                        <button type="button" onClick={() => removeEmailToInvite(email)}>
-                            <X className="size-5 text-zinc-400" />
-                        </button>
-                    </div>
-                ))}
-            </div>
-
-            <form onSubmit={addNewEmailToInvite} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                <div className="px-2 flex items-center gap-2 flex-1">
-                    <AtSign className="text-zinc-400 size-5" />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Digite o e-mail do convidado"
-                        className="bg-transparent text-lg placeholder-zinc-400 w-99 outline-none" />
-                </div>
-
-                <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 cursor-pointer">
-                    Convidar
-                    <Plus className="size-5" />
-                </button>
-            </form>
+          <p className="text-sm text-zinc-400">
+            Os convidados irão receber e-mails para confirmar a participação na viagem.
+          </p>
         </div>
-    </div>;
-}
+        
+        <div className="flex flex-wrap gap-2">
+          {emailsToInvite.map(email => {
+            return (
+              <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                <span className="text-zinc-300">{email}</span>
+                <button type="button">
+                  <X onClick={() => removeEmailFromInvites(email)} className="size-4 text-zinc-400" />
+                </button>
+              </div>
+              )
+            }
+          )}
+        </div>
+        
+        <div className="w-full h-px bg-zinc-800" />
 
-export default InviteGuestsModal;
+        <form onSubmit={addNewEmailToInvite} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+          <div className="px-2 flex items-center flex-1 gap-2">
+            <AtSign className="text-zinc-400 size-5" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite o email do convidado"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <Button type="submit">
+            Convidar
+            <Plus className="size-5" />
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
--- Plann.er/src/pages/Create-Trip/index.tsx
+++ Plann.er/src/pages/Create-Trip/index.tsx
@@ -1,131 +1,125 @@
-import Logo from "../../assets/Logo.svg";
-
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { InviteGuestsModal } from "./Invite-guests-modal";
-import { ConfirmeTripModal } from "./Confirme-Trip-Modal";
-import { DestinationAmdDateStep } from "./Steps/destination-amd-date-step";
-import { InviteGuestsSteps } from "./Steps/invite-guests-steps";
+import { ConfirmTripModal } from "./Confirme-Trip-Modal";
+import { DestinationAndDateStep } from "./Steps/destination-amd-date-step";
+import { InviteGuestsStep } from "./Steps/invite-guests-steps";
 
-function CreateTripPage() {
-  const navigate = useNavigate();
-
-  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
-  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
-  const [isConfirmeTripModalOpen, setIsConfirmeTripModalOpen] = useState(false);
-
-  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([]);
+export function CreateTripPage() {
+  const navigate = useNavigate()
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    'diego@rocketseat.com.br',
+    'john@acme.com'
+  ])
 
   function openGuestsInput() {
-    setIsGuestsInputOpen(true);
+    setIsGuestsInputOpen(true)
   }
 
   function closeGuestsInput() {
-    setIsGuestsInputOpen(false);
+    setIsGuestsInputOpen(false)
   }
 
   function openGuestsModal() {
-    setIsGuestsModalOpen(true);
+    setIsGuestsModalOpen(true)
   }
 
   function closeGuestsModal() {
-    setIsGuestsModalOpen(false);
+    setIsGuestsModalOpen(false)
   }
 
-  function openConfirmeTripModal() {
-    setIsConfirmeTripModalOpen(true);
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true)
   }
 
-  function closeConfirmeTripModal() {
-    setIsConfirmeTripModalOpen(false);
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false)
   }
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const email = data.get("email") as string;
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
 
     if (!email) {
-      return;
+      return
     }
 
     if (emailsToInvite.includes(email)) {
-      return alert("Este e-mail já foi adicionado");
+      return
     }
 
-    setEmailsToInvite((state) => [...state, email]);
+    setEmailsToInvite([
+      ...emailsToInvite,
+      email
+    ])
 
-    event.currentTarget.reset();
+    event.currentTarget.reset()
   }
 
-  function removeEmailToInvite(email: string) {
-    setEmailsToInvite((state) =>
-      state.filter((stateEmail) => stateEmail !== email)
-    );
+  function removeEmailFromInvites(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+
+    setEmailsToInvite(newEmailList)
   }
 
   function createTrip(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    navigate("/tripes/123");
+    event.preventDefault()
+
+    navigate('/trips/123')
   }
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-[url('./assets/bg.png')]  bg-center">
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-3">
-          <img src={Logo} alt="" />
+          <img src="/logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-lg">
-            Convide seus amigos planeje sua próxima viagem
+            Convide seus amigos e planeje sua próxima viagem!
           </p>
         </div>
 
         <div className="space-y-4">
-          
-          <DestinationAmdDateStep 
+          <DestinationAndDateStep 
+            closeGuestsInput={closeGuestsInput}
             isGuestsInputOpen={isGuestsInputOpen}
             openGuestsInput={openGuestsInput}
-            closeGuestsInput={closeGuestsInput}
           />
 
           {isGuestsInputOpen && (
-            <InviteGuestsSteps 
+            <InviteGuestsStep 
               emailsToInvite={emailsToInvite}
+              openConfirmTripModal={openConfirmTripModal}
               openGuestsModal={openGuestsModal}
-              openConfirmeTripModal={openConfirmeTripModal}
             />
           )}
         </div>
 
         <p className="text-sm text-zinc-500">
-          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
-          <br /> com nossos{" "}
-          <a href="#" className="text-zinc-300 underline">
-            termos de uso
-          </a>{" "}
-          e{" "}
-          <a className="text-zinc-300 underline" href="#">
-            políticas de privacidade.
-          </a>
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
         </p>
       </div>
+
       {isGuestsModalOpen && (
-        <InviteGuestsModal
+        <InviteGuestsModal 
           emailsToInvite={emailsToInvite}
           addNewEmailToInvite={addNewEmailToInvite}
           closeGuestsModal={closeGuestsModal}
-          removeEmailToInvite={removeEmailToInvite}
+          removeEmailFromInvites={removeEmailFromInvites}
         />
       )}
 
-      {isConfirmeTripModalOpen && (
-        <ConfirmeTripModal
-          closeConfirmeTripModal={closeConfirmeTripModal}
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal 
+          closeConfirmTripModal={closeConfirmTripModal}
           createTrip={createTrip}
         />
       )}
     </div>
   );
 }
-
-export default CreateTripPage;
